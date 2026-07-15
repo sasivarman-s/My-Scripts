84296944715d70df437585461896a3e6b5ae7404
sasivarman-s <sureshraman2680@gmail.com>
2026-07-15 20:48:26 +0530

    code written

---
+++ DOM-basic/index.js
@@ -0,0 +1,8 @@
+var h1 = document.getElementById("Heading")
+var h2 = document.getElementById("greet")
+var btn = document.getElementById("btn")
+function change(){
+    h1.textContent="Bye world!..."
+    h2.textContent="See you Again!..."
+    btn.textContent="changed"
+}

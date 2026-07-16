867da71a2e018b010ba33db79be65086939d0905
sasivarman-s <sureshraman2680@gmail.com>
2026-07-16 13:03:22 +0530

    code written

---
+++ instantprint/print.js
@@ -0,0 +1,8 @@
+// code
+
+function show() {
+    var input = document.getElementById("inpt")
+    var textinput = input.value
+    var para = document.getElementById("show")
+    para.textContent = textinput
+}

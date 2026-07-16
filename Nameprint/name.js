1c1cae2a76d07da809b6ee08a9ca85dba85ef9e9
sasivarman-s <sureshraman2680@gmail.com>
2026-07-16 11:56:08 +0530

    code written

---
+++ Nameprint/name.js
@@ -0,0 +1,20 @@
+var h1 = document.getElementById("content")
+var btn1 = document.getElementById("btn1")
+var btntext1 = btn1.textContent
+var btn2 = document.getElementById("btn2")
+var btntext2 = btn2.textContent
+var btn3 = document.getElementById("btn3")
+var btntext3 = btn3.textContent
+function change1(){
+    h1.textContent=btntext1
+}
+function change2(){
+    h1.textContent=btntext2
+}
+function change3(){
+    h1.textContent=btntext3
+}
+
+
+
+

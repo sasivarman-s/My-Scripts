ff9112c052afc2749dfe17f6c2cf53f63f25c39f
sasivarman-s <sureshraman2680@gmail.com>
2026-07-16 12:40:55 +0530

    code written

--- colorchange/color.js
+++ colorchange/color.js
@@ -1,4 +1,16 @@
 var h1 = document.getElementById("content")
 function cred(){
-    h1.style.backgroundColor="Red" 
+    h1.style.color="red" 
+}
+function cyellow(){
+    h1.style.color="yellow" 
+}
+function cgreen(){
+    h1.style.color="green" 
+}
+function corange(){
+    h1.style.color="orange" 
+}
+function cgray(){
+    h1.style.color="gray" 
 }

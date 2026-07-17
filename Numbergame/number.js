eaf614dfe392e31b28fac733b6f3cfd1f0e82abb
sasivarman-s <sureshraman2680@gmail.com>
2026-07-17 22:46:24 +0530

    boilerplate written

--- Numbergame/number.js
+++ Numbergame/number.js
@@ -1,1 +1,20 @@
 // code now
+var num = document.getElementById("num")
+var add = document.getElementById("btnadd")
+var sub = document.getElementById("btnsub")
+var reset = document.getElementById("btnreset")
+add.onclick = ()=>{
+    num.textContent=parseInt(num)+1
+    console.log(num)
+    
+}
+sub.onclick = ()=>{
+    num.textContent = parseInt(num)-1
+    console.log(num)
+    
+}
+reset.onclick = ()=>{
+    num.textContent = parseInt(num)*0
+    console.log(num)
+    
+}

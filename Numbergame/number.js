562abe4b1281f378f23c31b643cc2eefe5010bfc
sasivarman-s <sureshraman2680@gmail.com>
2026-07-18 18:28:38 +0530

    code written

--- Numbergame/number.js
+++ Numbergame/number.js
@@ -4,17 +4,17 @@ var add = document.getElementById("btnadd")
 var sub = document.getElementById("btnsub")
 var reset = document.getElementById("btnreset")
 add.onclick = ()=>{
-    num.textContent=parseInt(num)+1
+    num.textContent=parseInt(num.textContent)+1
     console.log(num)
     
 }
 sub.onclick = ()=>{
-    num.textContent = parseInt(num)-1
+    num.textContent = parseInt(num.textContent)-1
     console.log(num)
     
 }
 reset.onclick = ()=>{
-    num.textContent = parseInt(num)*0
+    num.textContent = 0
     console.log(num)
     
 }

a17ce532ab9ded28fc5365e43e2577c25553aef4
sasivarman-s <sureshraman2680@gmail.com>
2026-07-18 19:15:26 +0530

    code written

--- Numbergame/number.js
+++ Numbergame/number.js
@@ -5,16 +5,19 @@ var sub = document.getElementById("btnsub")
 var reset = document.getElementById("btnreset")
 add.onclick = ()=>{
     num.textContent=parseInt(num.textContent)+1
+    num.style.color="green"
     console.log(num)
     
 }
 sub.onclick = ()=>{
     num.textContent = parseInt(num.textContent)-1
+    num.style.color="red"
     console.log(num)
     
 }
 reset.onclick = ()=>{
     num.textContent = 0
+    num.style.color="black"
     console.log(num)
     
 }

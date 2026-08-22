79999468ec0b1a37a4493f991087cec88653473e
sasivarman-s <sureshraman2680@gmail.com>
2026-08-22 20:08:01 +0530

    array methods implemented

---
+++ JS_Array_methods/Js_map.js
@@ -0,0 +1,9 @@
+var arr = [1,2,3,4,5,6,7,8,9,10];
+var data = arr.map((e,i)=>{
+    return e;
+})
+console.log(data); 
+/* output -> [
+  1, 2, 3, 4,  5,
+  6, 7, 8, 9, 10
+] */
---
+++ JS_Array_methods/filter.js
@@ -0,0 +1,4 @@
+var arr = [1,2,3,4,5,6,7,8,9,10];
+var filterdata = arr.filter((e)=>e%2==0);
+console.log(filterdata);
+// output -> [ 2, 4, 6, 8, 10 ]

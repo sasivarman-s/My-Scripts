fa52c322a55a6865fcc9e675c049ade17f40a32a
sasivarman-s <sureshraman2680@gmail.com>
2026-08-22 19:10:17 +0530

    destucturing implemented on object

---
+++ Js_Operators/JS_destructuring/object-d.js
@@ -0,0 +1,10 @@
+var user = {
+    name : "json😂",
+    age : 18,
+    mail : "json@gmail.com",
+    password : "dummy@185t87/585-j879(lkj)f8957"
+}
+
+var {name , password} = user;
+console.log(name," ",password);
+// output -> json😂   dummy@185t87/585-j879(lkj)f8957
